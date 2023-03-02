454230988450fef3cfd7d474ffc397ecfe6208f2
Saurabh Kaperwan <91174352+SaurabhKaperwan@users.noreply.github.com>
2023-03-02 19:04:30 +0530

    Update Construct_BT_from_Preorder_&_Inorder.cpp

--- Tree/Construct_BT_from_Preorder_&_Inorder.cpp
+++ Tree/Construct_BT_from_Preorder_&_Inorder.cpp
@@ -12,34 +12,48 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution 
 {
     public:
 
-        TreeNode* buildTreeUtil(vector<int>& preorder, int preStart, int preEnd, vector<int>& inorder, int inStart, int inEnd, map<int,int>& mp)
+        TreeNode* buildTreeUtil(vector<int>& preorder, vector<int>& inorder, int inorderStart, int inorderEnd, int preorderStart, int preorderEnd, map<int,int>& mp)
         {
-            if(preStart > preEnd || inStart > inEnd)
+            if(inorderStart > inorderEnd and preorderStart > preorderEnd )
                 return NULL;
-            
-            TreeNode* root = new TreeNode(preorder[preStart]);
-            int ele = mp[root->val];
-            int nEle = ele - inStart;
 
-            root -> left = buildTreeUtil(preorder, preStart + 1, preStart + nEle, inorder, inStart, ele - 1, mp);
+            TreeNode* node = new TreeNode(preorder[preorderStart]);
+            int index = mp[node->val];
+            int diff = index - inorderStart;
 
-            root -> right = buildTreeUtil(preorder, preStart + nEle + 1, preEnd, inorder, ele + 1, inEnd, mp);
-            return root;
+            node->left = buildTreeUtil(preorder, inorder, inorderStart, index - 1, preorderStart + 1, preorderStart + diff, mp);
+            node->right = buildTreeUtil(preorder, inorder, index + 1, inorderEnd, preorderStart + diff + 1, preorderEnd, mp);
+            return node;
         }
 
         TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) 
         {
-            int preStart = 0, preEnd = preorder.size() - 1;
-            int inStart = 0, inEnd = inorder.size() - 1;
+            int n = inorder.size();
+            int inorderStart = 0, inorderEnd = n - 1;
+            int preorderStart = 0, preorderEnd = n - 1;
 
             map<int,int> mp;
 
-            for(int i = inStart; i <= inEnd; i++)
+            for(int i = 0; i < n; i++)
+            {
                 mp[inorder[i]] = i;
-            return buildTreeUtil(preorder, preStart, preEnd, inorder, inStart, inEnd, mp);
+            }
+
+            return buildTreeUtil(preorder, inorder, inorderStart, inorderEnd, preorderStart, preorderEnd, mp);
         }
 };
